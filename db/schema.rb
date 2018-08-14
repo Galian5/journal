01d800ee9ead55c66bb3d4de38ae9c3e159c3331
Galian <galian5@wp.pl>
2018-08-14 14:19:37 +0200

    use webpack

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180814092703) do
+ActiveRecord::Schema.define(version: 20180814103518) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
