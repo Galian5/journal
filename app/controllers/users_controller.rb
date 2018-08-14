3efe6db8bb8f86cb4b81160ffe6c617e911846f2
Galian <galian5@wp.pl>
2018-08-14 11:38:06 +0200

    add users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      log_in @user
+      redirect_to @user
+      flash[:success] = 'User created successfully'
+    else
+      render 'new'
+    end
   end
 
-  def update
-  end
+  private
 
-  def edit
-  end
-
-  def destroy
-  end
-
-  def index
-  end
-
-  def show
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
